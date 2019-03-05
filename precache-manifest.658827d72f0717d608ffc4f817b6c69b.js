414c9351ff65e8afed25f09f406bb3cd9c26541a
maurera <42736637+maurera91@users.noreply.github.com>
2019-03-04 21:53:40 -0600

    Updates

---
+++ precache-manifest.658827d72f0717d608ffc4f817b6c69b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory_game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3157a09b9f8bc3f76a51",
+    "url": "/memory_game/static/js/runtime~main.3157a09b.js"
+  },
+  {
+    "revision": "fad5c74d6b9e81793dd0",
+    "url": "/memory_game/static/js/main.fad5c74d.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/memory_game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "fad5c74d6b9e81793dd0",
+    "url": "/memory_game/static/css/main.961bf325.chunk.css"
+  },
+  {
+    "revision": "59cd6f72b309b0180050db1a3124bd4f",
+    "url": "/memory_game/index.html"
+  }
+];
